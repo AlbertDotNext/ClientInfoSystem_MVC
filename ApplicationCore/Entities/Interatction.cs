aa2475614a7a4effad33deb4822c0bafb2d4fd72
Albert Wu <albertwu0001@gmail.com>
2021-07-26 11:29:58 -0700

    Add project files.

---
+++ ApplicationCore/Entities/Interatction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities
+{
+    public class Interatction
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public Client Clients { get; set; }
+        public int EmpId { get; set; }
+        public Employee Employees { get; set; }
+        public string IntType { get; set; }
+        public DateTime? IntDate { get; set; }
+        public string Remarks { get; set; }
+
+
+    }
+}
