38f25603a3e332761c7de0bcfeb85def3dc8eb17
Albert Wu <albertwu0001@gmail.com>
2021-07-26 15:51:59 -0700

    WIP update client view

---
+++ ClientInfoSystemMVC/Controllers/ClientsController.cs
@@ -0,0 +1,25 @@
+﻿using ApplicationCore.ServiceInterfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientInfoSystemMVC.Controllers
+{
+    public class ClientsController : Controller
+    {
+        private readonly IClientService _clientService;
+
+        public ClientsController(IClientService clientService)
+        {
+            _clientService = clientService;
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateClient()
+        {
+            return View();
+        }
+    }
+}
