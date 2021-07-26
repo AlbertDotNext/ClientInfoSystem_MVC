f2715893151e1a4cbc5b303b1acdac7b1f805745
Albert Wu <albertwu0001@gmail.com>
2021-07-26 11:38:43 -0700

    created dbset and modifiy appsettings

---
+++ Infrastructure/Data/CISDbContext.cs
@@ -0,0 +1,22 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class CISDbContext : DbContext
+    {
+        public CISDbContext(DbContextOptions<CISDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Interatction> Interatctions { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
