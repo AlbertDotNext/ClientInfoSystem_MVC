687b4bfa945583c09a194867b17cad9c87c493e4
Albert Wu <albertwu0001@gmail.com>
2021-07-26 14:05:28 -0700

    complete get all clients

---
+++ Infrastructure/Services/ClientService.cs
@@ -0,0 +1,40 @@
+﻿using ApplicationCore.Models;
+using ApplicationCore.RepositoryInterfaces;
+using ApplicationCore.ServiceInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class ClientService : IClientService
+    {
+        private readonly IClientRepository _clientRepository;
+        public ClientService(IClientRepository clientRepository)
+        {
+            _clientRepository = clientRepository;
+        }
+
+        public async Task<List<ClientResponseModel>> GetAllClients()
+        {
+            var clients = await _clientRepository.ListAllAsync();
+            var clientList = new List<ClientResponseModel>();
+            foreach (var client in clients)
+            {
+                clientList.Add(new ClientResponseModel
+                {
+                    Id = client.Id,
+                    Name = client.Name,
+                    Email = client.Email,
+                    Phones = client.Phones,
+                    Address = client.Address,
+                    AddedOn = client.AddedOn
+                });
+            }
+
+            return clientList;
+        }
+    }
+}
