795f3b2258645d6cd6f146216542c0de3cab4ef5
Albert Wu <albertwu0001@gmail.com>
2021-07-26 15:52:27 -0700

    update client service complete

--- Infrastructure/Services/ClientService.cs
+++ Infrastructure/Services/ClientService.cs
@@ -1,4 +1,6 @@
-﻿using ApplicationCore.Models;
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Exceptions;
+using ApplicationCore.Models;
 using ApplicationCore.RepositoryInterfaces;
 using ApplicationCore.ServiceInterfaces;
 using System;
@@ -17,6 +19,7 @@ namespace Infrastructure.Services
             _clientRepository = clientRepository;
         }
 
+        
         public async Task<List<ClientResponseModel>> GetAllClients()
         {
             var clients = await _clientRepository.ListAllAsync();
@@ -35,6 +38,34 @@ namespace Infrastructure.Services
             }
 
             return clientList;
+        }
+
+        public async Task<ClientResponseModel> UpdateClientById(ClientRequestModel model)
+        {
+            var dbClient = await _clientRepository.GetByIdAsync(model.Id);
+            if(dbClient == null)
+            {
+                throw new NotFoundException("The client is not exists, please add this client first");
+            }
+            var client = new Client
+            {
+                Id = model.Id,
+                Name = model.Name,
+                Phones = model.Phones,
+                Email = model.Email,
+                Address = model.Address
+            };
+
+            var createdClient = await _clientRepository.UpdateAsync(client);
+            var clientResponse = new ClientResponseModel
+            {
+                Id = createdClient.Id,
+                Name = createdClient.Name,
+                Phones = createdClient.Phones,
+                Address = createdClient.Address,
+                Email = createdClient.Email
+            };
+            return clientResponse;
         }
     }
 }
