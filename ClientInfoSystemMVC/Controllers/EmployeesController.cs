efd0d64cdea73f1467915e6f35faa0649effd699
Albert Wu <albertwu0001@gmail.com>
2021-07-27 01:09:25 -0700

    WIP razor view for employees and interactions

---
+++ ClientInfoSystemMVC/Controllers/EmployeesController.cs
@@ -0,0 +1,25 @@
+﻿using ApplicationCore.ServiceInterfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientInfoSystemMVC.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeesController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        public async Task<IActionResult> EmployeeList()
+        {
+            var employees = await _employeeService.GetAllEmps();
+            return View(employees);
+        }
+    }
+}
---
+++ ClientInfoSystemMVC/Controllers/InteractionsController.cs
@@ -0,0 +1,30 @@
+﻿using ApplicationCore.Exceptions;
+using ApplicationCore.ServiceInterfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientInfoSystemMVC.Controllers
+{
+    public class InteractionsController : Controller
+    {
+        private readonly IInteractionService _interactionService;
+
+        public InteractionsController(IInteractionService interactionService)
+        {
+            _interactionService = interactionService;
+        }
+
+        public async Task<IActionResult> InteractionsList()
+        {
+            var interactions = await _interactionService.GetInteractions();
+            if (!interactions.Any())
+            {
+                throw new NotFoundException("No interactions found");
+            }
+            return View(interactions);
+        }
+    }
+}
